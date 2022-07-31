e2083bf9f21ffbea026249d9fc3764f1c507da07
mmrichmond6 <mmrichmond6@gmail.com>
2022-07-31 11:44:52 -0500

    First part of Array Exercises Completed

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,12 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+        
+            int[] numbers = new int[6] {1, 1, 2, 3, 5, 8};
+
+            foreach(int number in numbers) {
+                Console.WriteLine(number);
+            }
         }
     }
 }
