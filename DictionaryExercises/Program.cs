edc5a065fc965af1b04bd933d0a9a827c97ebd9d
mmrichmond6 <mmrichmond6@gmail.com>
2022-07-31 18:40:54 -0500

    Finished Dictionary Practice

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace DictionaryExercises
 {
@@ -6,7 +8,36 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudentName;
+            Console.WriteLine("Enter your student's Name (or ENTER to finish):");
+
+            //Get student IDs and names
+            do
+            {
+                Console.WriteLine("Student Name:  ");
+                string input = Console.ReadLine();
+                newStudentName = input;
+
+                if (!Equals(newStudentName, ""))
+                {
+                    Console.WriteLine("Student ID:  ");
+                    int newStudentID = int.Parse(Console.ReadLine());
+                    students.Add(newStudentID, newStudentName);
+
+                    //Read in the newline before looping back
+                    //Console.ReadLine();
+                }
+
+            } while (!Equals(newStudentName, ""));            
+           
+                //Print class roster
+                Console.WriteLine("Class roster:");
+
+                foreach (KeyValuePair<int, string> student in students)
+                {
+                    Console.WriteLine("Student Name:  " + student.Value + " ID(" + student.Key + ")");
+                }
+            }
         }
     }
-}
